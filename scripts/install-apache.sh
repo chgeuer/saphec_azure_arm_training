3c573c06f17864178d4539833e991359e3cfccc3
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-02-20 11:33:24 +0100

    nginx

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-zypper --non-interactive install apache
-echo 'ServerName localhost' >> /etc/apache2/httpd.conf
-/usr/sbin/apachectl restart
+zypper --non-interactive install -y docker
+service docker start
+
+mkdir /var/lib/html
+cat > /var/lib/html/index.html <<-EOF
+	<html><body>
+	<p>Welcome</p>
+	</body></html>
+EOF
+
+docker run --name docker-nginx -p 80:80 -d -v /var/lib/html:/usr/share/nginx/html nginx
